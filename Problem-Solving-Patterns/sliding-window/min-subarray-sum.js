c81fe08ab2345888ed74c1acf81c5bcebf854fc4
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-11-12 08:15:57 +0530

    Problem Solving Patterns: sliding window

---
+++ Problem-Solving-Patterns/sliding-window/min-subarray-sum.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function called minSubArrayLen which accepts two parameters - an
+ * array of positive integers and a positive integer.
+ * This function should return the minimal length of a contiguous subarray of
+ * which the sum is greater than or equal to the integer passed to the function.
+ * If there isn't one, return 0 instead.
+ */
