26fe9956122b295d710e478f3d375aec4f8b9789
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-11-25 17:10:35 +0530

    Sorting Algorithms: Selection sort

---
+++ 4.Sorting Algorithms/selection-sort.js
@@ -0,0 +1,24 @@
+function swap(arr, idx1, idx2) {
+  const temp = arr[idx1]
+  arr[idx1] = arr[idx2]
+  arr[idx2] = temp
+}
+
+function selectionSort(arr) {
+  const length = arr.length
+  let minIndex
+  for (let i = 0; i < length; i++) {
+    minIndex = i
+    for (let j = i + 1; j < length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j
+      }
+    }
+    if (minIndex !== i) {
+      swap(arr, i, minIndex)
+    }
+  }
+  return arr
+}
+
+console.log(selectionSort([5, 1, 3, 2, 4]))
