c1d9cbafb470a3877d0e04b6377299e4bd924dd3
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-11-25 16:40:04 +0530

    Sorting Algoritms: Bubble sort

---
+++ 4.Sorting Algorithms/bubble-sort.js
@@ -0,0 +1,23 @@
+function swap(arr, idx1, idx2) {
+  const temp = arr[idx1]
+  arr[idx1] = arr[idx2]
+  arr[idx2] = temp
+}
+
+function bubbleSort(arr) {
+  const length = arr.length
+  let noSwaps = true
+  for (let i = length - 1; i > 0; i--) {
+    noSwaps = true
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1)
+        noSwaps = false
+      }
+    }
+    if (noSwaps) break
+  }
+  return arr
+}
+
+console.log(bubbleSort([5, 1, 3, 2, 4]))
