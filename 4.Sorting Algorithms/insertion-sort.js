4a50ced0fce77cc3b59a6fcd2bd106953b25aca8
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-11-25 20:07:09 +0530

    Sorting Algorithms: Insertion sort

---
+++ 4.Sorting Algorithms/insertion-sort.js
@@ -0,0 +1,17 @@
+function swap(arr, idx1, idx2) {
+  const temp = arr[idx1]
+  arr[idx1] = arr[idx2]
+  arr[idx2] = temp
+}
+
+function insertionSort(arr) {
+  const length = arr.length
+  for (let i = 1; i < length; i++) {
+    for (let j = i - 1; j >= 0 && arr[j + 1] < arr[j]; j--) {
+      swap(arr, j, j + 1)
+    }
+  }
+  return arr
+}
+
+console.log(insertionSort([5, 1, 3, 2, 4]))
